5ce338f6c2c4dea693a87689c18cdca233681de5
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-05-29 03:38:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(mus,inst){
   var arr=new Array();
   for(let i=0;i<mus.length;i++){
-    var str=mus[i]+inst[i];
+    var str=mus[i]+" plays "inst[i];
     arr.push(str);
   }
   return arr;
